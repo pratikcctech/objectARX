f6903674ec43351422302f63d0b34fca54bc760a
pratikcctech <149318512+pratikcctech@users.noreply.github.com>
2023-11-26 22:11:52 +0530

    exercise-2 added

---
+++ Exercise-2/exercise-2/exercise-2.h
@@ -0,0 +1,29 @@
+// exercise-2.h : main header file for the exercise-2 DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+#include <Windows.h>
+#include<arxHeaders.h>
+#include <dbents.h>
+
+// Cexercise2App
+// See exercise-2.cpp for the implementation of this class
+//
+
+class Cexercise2App : public CWinApp
+{
+public:
+	Cexercise2App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	static void AddLine();
+	DECLARE_MESSAGE_MAP()
+};
